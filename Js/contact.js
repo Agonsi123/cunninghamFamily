6d08bb3bb2cb0c99e1d4ae3841065efc22f37b03
Agonsi123 <agonsiudodirim@gmail.com>
2025-03-07 15:14:47 +0100

    changes to form

--- Js/contact.js
+++ Js/contact.js
@@ -91,3 +91,12 @@ whatsappIcon.addEventListener("mouseout", () => {
 });
 
 // Script for WhatsApp end
+
+// Script for form submission start
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+// Script for form submission end
